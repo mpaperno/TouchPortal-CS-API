dab4d6e026c78e5404d320ade2a579a15eb71c57
Max Paperno <max@wdg.us>
2024-11-25 22:57:08 -0500

    Update `TriggerEventStates` dictionary value type because `triggerEvent.states` values must be of string type.

--- TouchPortalSDK/Messages/Commands/TriggerEventCommand.cs
+++ TouchPortalSDK/Messages/Commands/TriggerEventCommand.cs
@@ -17,9 +17,13 @@ namespace TouchPortalSDK.Messages.Commands
 
     /// <summary> The event id to trigger. This event must be defined in the plugin's entry.tp declaration.</summary>
     public string EventId { get; set; }
-    /// <summary> This gets serialized to a JSON Object that holds key value pairs of data that are used within Touch Portal as Local States. </summary>
+    /// <summary> This gets serialized to a JSON Object that holds key value pairs of data that are used within Touch Portal as Local States. Optional.</summary>
     public TriggerEventStates States { get; set; } = null;
 
+    /// <summary> Construct a new TriggerEventCommand. </summary>
+    /// <param name="eventId">The event id to trigger.</param>
+    /// <param name="states">This optional argument gets serialized to a JSON Object that holds key value pairs of data that are used within Touch Portal as Local States.</param>
+    /// <exception cref="ArgumentNullException">If eventId is null or empty and `TouchPortalOptions.ValidateCommandParameters` is enabled.</exception>
     public TriggerEventCommand(string eventId, TriggerEventStates states = null)
     {
       if (TouchPortalOptions.ValidateCommandParameters && string.IsNullOrWhiteSpace(eventId))
@@ -36,9 +40,9 @@ namespace TouchPortalSDK.Messages.Commands
 
 
   /// <summary>
-  /// This is a `Dictionary&lt;string, object&gt;` typedef for usage in TriggerEventCommand.States message value.
+  /// This is a `Dictionary&lt;string, string&gt;` typedef for usage in TriggerEventCommand.States message value.
   /// </summary>
-  public sealed class TriggerEventStates : System.Collections.Generic.Dictionary<string, object>
+  public sealed class TriggerEventStates : System.Collections.Generic.Dictionary<string, string>
   { }
 
 }
